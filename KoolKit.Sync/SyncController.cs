b421cd4c3875338a93d57c3b49eac0069edbd528
Sam Mirza <sam@sammirza.co.uk>
2014-07-20 10:46:44 -0400

    Everything was VB?! Now it's C#. Added some things to read the ToolKit data.json file

---
+++ KoolKit.Sync/SyncController.cs
@@ -0,0 +1,33 @@
+﻿using Arendee.BTSyncLib;
+using Arendee.BTSyncLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoolKit.Sync {
+    public class SyncController {
+
+        private const string HOSTNAME = "localhost";
+        private const int TCP_PORT = 8888;
+        private const string USERNAME = "api";
+        private const string PASSWORD = "secret";
+        private const bool SELECTIVE_SYNC = false;
+
+        private BTSyncClient syncClient;
+
+        public SyncController() {
+            syncClient = new BTSyncClient(HOSTNAME, TCP_PORT, USERNAME, PASSWORD);
+        }
+
+        public void SetupSync(String folderToSync, String folderSecret) {
+            // add folder to sync client
+            var addFolderTask = syncClient.AddFolder(folderToSync, folderSecret, SELECTIVE_SYNC);
+            // block until task is complete
+            addFolderTask.Wait();
+        }
+
+
+    }
+}
